c1a702b6797d32e3ce74fd7ffeddec6c90a0c27c
f6388600-ai <f6388600@gmail.com>
2025-12-03 22:42:17 +0600

    Update cmds.js

--- scripts/cmds/cmds.js
+++ scripts/cmds/cmds.js
@@ -6,11 +6,11 @@ const ITEMS_PER_PAGE = 10;
 module.exports.config = {
   name: "cmdstore",
   aliases: ["cmds", "cs"],
-  author: "MahMUD",
+  author: "HaSaN",
   role: 0,
   version: "1.7",
   description: {
-    en: "Commands Store of MahMUD",
+    en: "Commands Store of HaSAM",
   },
   countDown: 3,
   category: "general",
@@ -57,7 +57,7 @@ module.exports.onStart = async function ({ api, event, args }) {
     const cmdsToShow = finalArray.slice(startIndex, endIndex);
 
     let msg = `╭─‣ 𝐓𝐔𝐌𝐀𝐑 𝐉𝐀𝐌𝐀𝐈
-    🎀\n├‣ 𝐀𝐃𝐌𝐈𝐍: 𝐇𝐚𝐬𝐚𝐧 𝐌𝐚𝐡𝐦𝐨𝐮𝐝\n├‣ 𝐓𝐎𝐓𝐀𝐋 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒: ${finalArray.length}\n╰────────────◊\n`;
+    🎀\n├‣ 𝐀𝐃𝐌𝐈𝐍: 𝐇𝐀𝐒𝐀𝐍\n├‣ 𝐓𝐎𝐓𝐀𝐋 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒: ${finalArray.length}\n╰────────────◊\n`;
 
     cmdsToShow.forEach((cmd, index) => {
       msg += `╭─‣ ${startIndex + index + 1}: ${cmd.cmd}\n├‣ Author: ${cmd.author}\n├‣ Update: ${cmd.update}\n╰────────────◊\n`;
